d39c300efe5178e08b958e5115b4679ffd2d2051
coderFeedForwardAlg <gaspersscottma@gmail.com>
2025-04-22 17:14:57 -0700

    small changes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import axios from 'axios';
 
 
 function App() {
-  const [messeges, setMesseges] = useState(["ai messege"]);
+  const [messeges, setMesseges] = useState([]);
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState("");
+  const [loading, setLoading] = useState(false); // bad use of semiphor, consiter acters
   const question_changed = (e) => {
     setQuestion(e.target.value);
   }
 
   const ask_question = () => {
+    // if(loading):
+    //   return
     setMesseges([...messeges, question]);
     axios.get(`http://localhost:8000/${question}`).then(response => setAnswer(response.data.res)).catch(err => setAnswer("an error accurd connecting to the server :("));
+    setQuestion("");
   }
   
   useEffect(
@@ -26,16 +30,31 @@ function App() {
 
   return (
     <div>
+      {/*<p className="text-lg font-bold">This is for beta testing. Looking for feedback/suggestions on:</p>
+      <ul>
+        <li>colors (something better than red and blue)</li>
+        <li> background-color</li>
+        <li> anything else </li>
+      </ul>
+      <p className="text-lg font-bold"> known issues</p>
+      <ul> 
+        <li>bot has no memory (will not remember previous parts of conversation)</li>
+        <li>empty message causes error</li>
+        <li></li>
+      </ul>*/}
     <h1 className="text-3xl font-bold">
       This is a place where you can ask about max gaspers scott.
     </h1>
-      <input value={question} onChange={question_changed}/>
-      <button onClick={ask_question} > send </button>
-      {messeges.map((text, index) => (
-        <div key={index}>  
-          <p className={`text-lg ${(index % 2 == 0) ? "bg-blue-300": "bg-red-300"} rounded-xl pl-2`}> {text} </p>
+      {messeges.map((text, index) => ( 
+        <div key={index} className={`${index % 2 != 0 ? "flex justify-end" : "pr-24"}`}>  
+          <p className={`text-lg ${(index % 2 != 0) ? "bg-blue-300": "bg-red-300"} rounded-xl px-4`}> { text} </p>
           </div>
-      ))} 
+      ))}
+      <div className="flex absolute bottom-0 right-0 p-4 ">
+      <input value={question} onChange={question_changed} className="border border-gray-400 w-full"/>
+      <button onClick={ask_question} > send </button>
+      </div>
+
     </div>
 
 );
